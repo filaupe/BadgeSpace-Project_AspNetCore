e1da76b1abe4be76ad5d16fb532c11ebe2864179
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-10-06 18:41:34 -0300

    Back do back

---
+++ BadgeSpace/Controllers/ApiController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BadgeSpace.Controllers
+{
+    public class ApiController1 : Controller
+    {
+        [Controller]
+        [Route("[Controller]")]
+        public class ApiController
+        {
+            public string oi()
+            {
+                return "Hello World";
+            }
+        }
+    }
+}
