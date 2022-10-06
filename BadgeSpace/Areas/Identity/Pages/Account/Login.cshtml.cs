56a9f5989ff449a8a4de5f506e48986d02de6d62
João Pedro <pedrosant1905@gmail.com>
2022-10-06 18:31:59 -0300

    Implementação do front-end

--- BadgeSpace/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BadgeSpace/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace BadgeSpace.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = " Lembrar da senha?")]
             public bool RememberMe { get; set; }
         }
 
