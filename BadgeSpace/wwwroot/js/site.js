fd16268c32021226ba61c831acc55cd448dd6126
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-10-08 11:35:37 -0300

    .

--- BadgeSpace/wwwroot/js/site.js
+++ BadgeSpace/wwwroot/js/site.js
@@ -16,6 +16,7 @@ CheckBox.onchange = function () {
 }
 
 
+
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
