c9479f1e39a79cc19f2199d4b2d3edf99223d483
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-10-11 01:12:46 -0300

    .

--- BadgeSpace/Extensions/AlunosController.cs
+++ BadgeSpace/Extensions/AlunosController.cs
@@ -17,22 +17,32 @@ namespace BadgeSpace.Extensions
 
         public AlunosController(ApplicationDbContext context) => _context = context;
 
-        [HttpGet("email={email}/{CPF?}")]
+        [HttpGet("email={email}/{CPF?}/{ID?}")]
         [Authorize(Roles = nameof(Roles.EMPRESS))]
-        public async Task<IActionResult> Listar(string email, string? CPF)
+        public async Task<IActionResult> Listar(string email, string? CPF, int? ID)
         {
-            if (CPF != null && CPF != "")
+            if (ID.HasValue && ID != 0)
             {
-                var student = await _context.Students.FirstOrDefaultAsync(c => c.AlunoCPF == CPF);
+                var student = await _context.Students.FirstOrDefaultAsync(c => c.Id == ID);
                 if (student == null) return NotFound(new { message = "Aluno Inválido." });
 
-                return Ok(new { student.Id, student.AlunoCPF, student.Curso });
+                return Ok(new { student.Id, student.NomeAluno, student.AlunoCPF, student.Curso });
+            }
+                
+
+            if (CPF != null && CPF != "")
+            {
+                var students = _context.Students
+                    .Where(c => c.AlunoCPF == CPF && c.EmpresaId == email)
+                    .Select(c => new { c.Id, c.NomeAluno, c.AlunoCPF, c.Curso } );
+                if (students == null) return NotFound(new { message = "Aluno Inválido." });
+
+                return Ok(students);
             }
 
-            var dados = await _context.Students
+            var dados = _context.Students
                 .Where( c => c.EmpresaId == email )
-                .Select( c => new { c.Id, c.AlunoCPF, c.Curso } )
-                .FirstOrDefaultAsync();
+                .Select( c => new { c.Id, c.NomeAluno, c.AlunoCPF, c.Curso });
 
             if (dados == null) return NotFound(new { message = "A lista está vazia." });
 
@@ -41,11 +51,11 @@ namespace BadgeSpace.Extensions
 
         [HttpGet("{CPF}")]
         [Authorize(Roles = nameof(Roles.STUDENT))]
-        public async Task<IActionResult> ListarCertificados(string? CPF)
+        public IActionResult ListarCertificados(string? CPF)
         {
             if (CPF != null)
             {
-                var student = await _context.Students.FirstOrDefaultAsync(c => c.AlunoCPF == CPF);
+                var student = _context.Students.Where(x => x.AlunoCPF == CPF);
                 if (student == null) return NotFound(new { message = "Aluno Inválido." });
 
                 return Ok(student);
@@ -73,20 +83,23 @@ namespace BadgeSpace.Extensions
             return Ok(Student);
         }
 
-        [HttpDelete("deletar={codigo}")]
+        [HttpDelete("deletar={CPF}/{ID?}")]
         [Authorize(Roles = nameof(Roles.EMPRESS))]
-        public async Task<IActionResult> Remover(int codigo)
+        public async Task<IActionResult> Remover(string CPF, int? ID)
         {
-            var Student = await filtrar(codigo);
-            if (Student == null) return NotFound();
+            var student = ID.HasValue && ID != 0 
+                ? await _context.Students.FirstOrDefaultAsync(c => c.Id == ID) : await filtrar(CPF);
+            if (student == null) return NotFound(new { message = "Aluno Inválido." });
 
-            _context.Students.Remove(Student);
+            _context.Students.Remove(student);
             await _context.SaveChangesAsync();
-            return Ok();
+
+            return Ok(new { message = "Aluno Deletado." });
         }
 
         #region Private Methods
-        private async Task<StudentModel?> filtrar(int codigo) => await _context.Students.FindAsync(codigo);
+        private async Task<StudentModel?> filtrar(string CPF) 
+            => await _context.Students.FirstOrDefaultAsync(c => c.AlunoCPF == CPF);
         #endregion
     }
 }
