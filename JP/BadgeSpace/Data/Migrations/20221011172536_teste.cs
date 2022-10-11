f5ecaa2e7b0d75465432a3ebddcc4b4b0dbd3795
João Pedro <pedrosant1905@gmail.com>
2022-10-11 17:39:23 -0300

    Front-end 50% feito

---
+++ JP/BadgeSpace/Data/Migrations/20221011172536_teste.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BadgeSpace.Data.Migrations
+{
+    public partial class teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CPF_CNPJ",
+                table: "AspNetUsers",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPF_CNPJ",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "AspNetUsers",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: true);
+        }
+    }
+}
