0fcc6758b4a161720a21d5913c9df6c038035df4
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-10-13 15:57:18 -0300

    Back-End e backups

---
+++ Back-End_Backup/Models/ApiLoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BadgeSpace.Models
+{
+    public class ApiLoginModel
+    {
+        public string Login { get; set; } = null!;
+        public string Senha { get; set; } = null!;
+    }
+}
---
+++ BadgeSpace/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using BadgeSpace.Data;
+using BadgeSpace.Models;
+using BadgeSpace.Models.Enums;
+using BadgeSpace.Utils.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BadgeSpace.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index() => View();
+
+        public IActionResult API() => View();
+
+        public IActionResult Sobre() => View();
+
+        public IActionResult Contato() => View();
+
+        [Authorize(Roles = nameof(Roles.STUDENT))]
+        public IActionResult Dashboard()
+        {
+            var (logado, user) = CheckIfUserIsValid.IsUserValid(_context.Users, User);
+
+            if (logado)
+            {
+                ViewBag.CPF = user!.CPF_CNPJ;
+                return View(_context.Students.AsEnumerable());
+            }
+
+            return Unauthorized();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+            =>  View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
