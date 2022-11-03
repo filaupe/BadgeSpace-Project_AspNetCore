75ccca198000f1f71ae5d9af042f775ea02bb8fa
J3ipy <97753966+J3ipy@users.noreply.github.com>
2022-11-03 18:38:24 -0300

    Implementação do front

--- BadgeSpaceDDD/src/Web/wwwroot/js/site.js
+++ BadgeSpaceDDD/src/Web/wwwroot/js/site.js
@@ -15,14 +15,6 @@ CheckBox.onchange = function () {
     }
 }
 
-/* ScrollUp */
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-
-}
-window.addEventListener('scroll', scrollUp)
-
 /*Rolagem Suave*/
 
 const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
