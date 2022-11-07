87481df9f1b98f8f0d755da48728b5eda128e3e4
J3ipy <97753966+J3ipy@users.noreply.github.com>
2022-11-07 17:17:23 -0300

    Mudança no front-end

--- BadgeSpaceDDD/src/Web/wwwroot/js/scrollup.js
+++ BadgeSpaceDDD/src/Web/wwwroot/js/scrollup.js
@@ -1,18 +1,18 @@
-//SCROLLUP
-function calcScrollValue() {
-    let scrollProgress = document.getElementById("pogress");
-    let progressValue = document.getElementById("progress-value");
-    let pos = document.documentElement.scrollTop;
-    let calcHeight = document.documentElement.scrollHeight -
-        document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-    if (pos > 100) {
-        scrollProgress.style.display = "grid";
-    } else {
-        scrollProgress.style.display = "none";
-    }
-    scrollProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #6970dd ${scrollValue}%)`;
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
 }
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
