11d0acf681f92d69e32a8bd5b58d16a5f62b2c7d
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-11-16 13:18:00 -0300

    99%

--- BadgeSpace/DomainDrivenDesign/Domain/Argumentos/Usuario/UsuarioRequest.cs
+++ BadgeSpace/DomainDrivenDesign/Domain/Argumentos/Usuario/UsuarioRequest.cs
@@ -1,4 +1,5 @@
 ﻿using Domain_Driven_Design.Domain.Argumentos.Base;
+using Domain_Driven_Design.Domain.Recursos.CustomValidationAttributes;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -14,15 +15,16 @@ namespace Domain_Driven_Design.Domain.Argumentos.Usuario
         [Required(ErrorMessage = "A área Email é obrigatória")]
         [EmailAddress(ErrorMessage = "Adicione um Email válido")]
         [DisplayName("Email")]
-        public string Email { get; set; }
+        //[EmailExist()]
+        public string Email { get; set; } = string.Empty;
 
-        public string? NormalizedEmail { get; set; }
+        public string NormalizedEmail { get; set; } = string.Empty;
 
         [Remote(action: "VerificarCPFouCNPJ", controller: "Validation")]
         [Required(ErrorMessage = "A área de CPF ou CNPJ é obrigatória")]
         [DisplayName("CPF")]
         [RegularExpression("(^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$)|(^\\d{2}\\.\\d{3}\\.\\d{3}\\/\\d{4}\\-\\d{2}$)", ErrorMessage = "A área não está de acordo com os padrões de formatação")]
-        public string CPFouCNPJ { get; set; }
+        public string CPFouCNPJ { get; set; } = string.Empty;
 
         public byte[]? Imagem { get; set; }
 
@@ -30,13 +32,13 @@ namespace Domain_Driven_Design.Domain.Argumentos.Usuario
         [DisplayName("Senha")]
         [DataType(DataType.Password)]
         [MinLength(8, ErrorMessage = "A área deve haver ao menos 8 caracteres")]
-        public string Senha { get; set; }
+        public string Senha { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "A área Confirmar Senha é obrigatória")]
         [DataType(DataType.Password)]
         [DisplayName("Confirmar Senha")]
         [Compare(nameof(Senha), ErrorMessage = "As senhas não são iguais")]
-        public string ConfirmarSenha { get; set; }
+        public string ConfirmarSenha { get; set; } = string.Empty;
 
         [DisplayName("Usuário / Empresa")]
         public bool Claim { get; set; }
--- BadgeSpace/DomainDrivenDesign/Domain/Recursos/CustomValidationAttributes/EmailExistAttribute.cs
+++ BadgeSpace/DomainDrivenDesign/Domain/Recursos/CustomValidationAttributes/EmailExistAttribute.cs
@@ -1,11 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Domain_Driven_Design.Infra;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain_Driven_Design.Domain.Recursos.CustomValidationAttributes
 {
     public class EmailExistAttribute : ValidationAttribute
     {
+        //private readonly ApplicationDbContext _context;
+        
+        //protected EmailExistAttribute() { }
+        //public EmailExistAttribute(ApplicationDbContext context) => _context = context;
+
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-            => "" == value!.ToString()!.ToUpper()
+            => "Consulta do banco" == value!.ToString()!.ToUpper()
                 ? new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName))
                     : ValidationResult.Success;
     }
